5e672085cb2a2ba4474c8b140171760a96a1b98c
Adam <63083988+amwooten@users.noreply.github.com>
2020-04-08 15:26:25 -0400

    Update antennaGain.m

--- Communications/Adam_Wooten/antennaGain.m
+++ Communications/Adam_Wooten/antennaGain.m
@@ -1,6 +1,7 @@
 function G_T = antennaGain(A_eff,lambda)
 %% Code to calculate gain for transmitting antenna
 % Written by Eric Smith
+% Used by linkBudget.m used in analysis done by Adam Wooten
 % A_eff = effective area of antenna
 % lambda = wavelength of signal
 % the units of A_eff must be equal to the units of lambda^2
