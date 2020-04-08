041ee785b1d08faef68ade4aec2c6662a9378fc8
Adam <63083988+amwooten@users.noreply.github.com>
2020-04-08 15:19:33 -0400

    Update spaceLoss.m

--- Communications/Adam_Wooten/spaceLoss.m
+++ Communications/Adam_Wooten/spaceLoss.m
@@ -2,6 +2,7 @@ function FSL = spaceLoss(d,lambda)
 % Calculates the Free Space loss in dB of a signal of wavelength lambda,
 % that travels the distance d.
 % Written by Eric Smith
+% Used by Link Budget written by Adam Wooten
 
 FSL = pow2db((4*pi*d/lambda)^2);
 end
