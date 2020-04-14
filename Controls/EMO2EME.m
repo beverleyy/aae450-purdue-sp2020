0a7fae0d41b1298a9cde6211ce7885988a8c0c56
AlexanderJChapa <52256731+AlexanderJChapa@users.noreply.github.com>
2020-04-13 20:54:41 -0400

    Delete EMO2EME.m

--- Controls/EMO2EME.m
+++
@@ -1,9 +0,0 @@
-function output = EMO2EME(input, E)
-
-EMO2EME = [1,0,0;0,cosd(E),sind(E);0,-sind(E),cosd(E)];
-x1 = [];
-for count = 1:length(input(:,1))
-    converted = EMO2EME*input(count,:)';
-    x1 = [x1, converted];
-end
-output = x1;
